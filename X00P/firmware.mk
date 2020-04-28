07fbced67cb9e2d6f149ff360bbe646e4135b1c8
althafvly <althafvly@gmail.com>
2020-04-28 22:57:59 +0530

    initialise

---
+++ X00P/firmware.mk
@@ -0,0 +1,19 @@
+# Firmware
+PRODUCT_COPY_FILES += \
+    vendor/firmware-asus/X00P/8917_adspso_P.bin:install/firmware-update/8917_adspso_P.bin \
+    vendor/firmware-asus/X00P/8917_cmnlib.mbn:install/firmware-update/8917_cmnlib.mbn \
+    vendor/firmware-asus/X00P/8917_cmnlib64.mbn:install/firmware-update/8917_cmnlib64.mbn \
+    vendor/firmware-asus/X00P/8917_emmc_appsboot.mbn:install/firmware-update/8917_emmc_appsboot.mbn \
+    vendor/firmware-asus/X00P/8917_keymaster64.mbn:install/firmware-update/8917_keymaster64.mbn \
+    vendor/firmware-asus/X00P/8917_NON-HLOS_P.bin:install/firmware-update/8917_NON-HLOS_P.bin \
+    vendor/firmware-asus/X00P/8917_rpm.mbn:install/firmware-update/8917_rpm.mbn \
+    vendor/firmware-asus/X00P/8917_sbl1.mbn:install/firmware-update/8917_sbl1.mbn \
+    vendor/firmware-asus/X00P/8937_adspso_P.bin:install/firmware-update/8937_adspso_P.bin \
+    vendor/firmware-asus/X00P/8937_cmnlib.mbn:install/firmware-update/8937_cmnlib.mbn \
+    vendor/firmware-asus/X00P/8937_cmnlib64.mbn:install/firmware-update/8937_cmnlib64.mbn \
+    vendor/firmware-asus/X00P/8937_emmc_appsboot.mbn:install/firmware-update/8937_emmc_appsboot.mbn \
+    vendor/firmware-asus/X00P/8937_keymaster64.mbn:install/firmware-update/8937_keymaster64.mbn \
+    vendor/firmware-asus/X00P/8937_NON-HLOS_P.bin:install/firmware-update/8937_NON-HLOS_P.bin \
+    vendor/firmware-asus/X00P/8937_rpm.mbn:install/firmware-update/8937_rpm.mbn \
+    vendor/firmware-asus/X00P/8937_sbl1.mbn:install/firmware-update/8937_sbl1.mbn \
+    vendor/firmware-asus/X00P/asusfw_raw.img:install/firmware-update/asusfw_raw.img
